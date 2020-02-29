479a748932acbc8dccc0cbb7d36304b0f224dbf4
jack <os-shenxj@jsfund.cn>
2020-02-29 20:59:52 +0800

    feat:add vue component in react component

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,29 @@
-const { override } = require("customize-cra");
-const path = require("path");
+const {
+  override,
+  addWebpackPlugin,
+  addBabelPlugins
+} = require("customize-cra");
+// 参考：https://github.com/timsayshey/vuera-react-vue-demo-app/commit/e169832cce222c201421db005fb9f924f9b80999
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const addWebpackModuleRule = () => config => {
+  // modify rule of file-loader
+  const rule = config.module.rules[2];
+  rule.oneOf[rule.oneOf.length - 1].exclude = [
+    /\.(js|mjs|jsx|ts|tsx|vue)$/,
+    /\.html$/,
+    /\.json$/
+  ];
 
-module.exports = override();
+  // add vue-loader
+  config.module.rules.push({
+    test: /\.vue$/,
+    loader: "vue-loader"
+  });
+  return config;
+};
+
+module.exports = override(
+  addBabelPlugins("vuera/babel"),
+  addWebpackPlugin(new VueLoaderPlugin()),
+  addWebpackModuleRule()
+);
