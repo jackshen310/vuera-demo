abeeafd47a768f504f0ef368abb919bb9d127409
jack <os-shenxj@jsfund.cn>
2020-03-28 23:06:38 +0800

    feat: add some code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import MyVueComponent from "./MyVueComponent.vue";
 
-export default () => (
-  <div>
-    <MyVueComponent message="Hello from Vue!!" />
-  </div>
-);
+export default () => {
+  const [value, setValue] = useState();
+  const [message, setMessage] = useState("Hello from Vue!!");
+  return (
+    <div>
+      <input
+        value={message}
+        onChange={e => {
+          setMessage(e.target.value);
+        }}
+      ></input>
+      <MyVueComponent message={message} onSubmit={setValue} />
+      <p>{value}</p>
+    </div>
+  );
+};
--- src/MyVueComponent.vue
+++ src/MyVueComponent.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <h4>Hello from Vue!!!! {{ this.clicky }}</h4>
+    <h4>{{this.message}} {{ this.clicky }}</h4>
+    <input @change="handleChange" />
     <button @click="clickMe()">{{ this.clicky ? "Click" : "Clack" }}</button>
   </div>
 </template>
 <script>
 export default {
   name: "Testing",
+  props: ["message", "onSubmit"],
   methods: {
     clickMe() {
       this.clicky = !this.clicky;
+    },
+    handleChange(e) {
+      this.onSubmit(e.target.value);
     }
   },
   data() {
     return {
       clicky: true
     };
+  },
+  created: function() {
+    // `this` 指向 vm 实例
+    console.log("message: " + this.message);
   }
 };
 </script>
